00ba48f9651da6ed18c08bbb6e3fca9496b0e307
Kwon Chan Yong <yong9545492@gmail.com>
2018-07-04 15:30:00 +0900

    11399번 문제입니다.
    입력받은 시간들 N개만큼을 timeArray 배열에 저장하였고, sorting 기법으로 배열 내 원소들을 오름차순으로 정리하였으며, timeArray 내 각 원소값을 재설정하였고, 각 원소의 합을 출력하였습니다.

---
+++ KCY/question11399_KCY.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+	int* timeArray = new int[N];
+
+	for (int i = 0; i < N; i++)
+	{
+		int time;
+		cin >> time;
+		timeArray[i] = time;
+	}
+	sort(timeArray, timeArray+N);
+	
+	for (int j = 1; j < N; j++) 
+	{
+		timeArray[j] += timeArray[j-1];
+	}
+
+	int sum = 0; // 기다린 시간 합
+	for (int k = 0; k < N; k++)
+	{
+		sum += timeArray[k];
+	}
+	cout << sum << endl;
+	return 0;
+}
